2ac6bad3f8180493e38e7d7620561bfc5e745dbd
al269161@edu.uaa.mx <MASTER@DESKTOP-PV7A9B7>
2021-05-04 23:25:15 -0500

    Rama desarrollo eliminada

--- src/capacitacion1/Capacitacion1.java
+++ src/capacitacion1/Capacitacion1.java
@@ -8,6 +8,7 @@ public class Capacitacion1 {
     public static void main(String[] args) {
         System.out.println("Version 1.1 final");
         System.out.println("Hola");
+        System.out.println("Rama eliminada de desarrollo");
     }
     
 }
